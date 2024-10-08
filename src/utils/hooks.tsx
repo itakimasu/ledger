ee13e3cb7eedbaf983358bbff42a2c725d2b3dfb
Steven Song <stevensong@ssong01.local>
2024-10-08 09:20:28 -0500

    missed save

--- src/utils/hooks.tsx
+++ src/utils/hooks.tsx
@@ -1,5 +1,6 @@
 import { httpBatchLink } from '@trpc/client';
 import { createTRPCNext } from '@trpc/next';
+import type { inferRouterInputs, inferRouterOutputs } from '@trpc/server';
 import type { AppRouter } from '../server/routers/_app';
 import superjson from 'superjson';
 
@@ -30,3 +31,6 @@ export const trpc = createTRPCNext<AppRouter>({
   ssr: false,
   transformer: superjson,
 });
+
+export type RouterInput = inferRouterInputs<AppRouter>;
+export type RouterOutput = inferRouterOutputs<AppRouter>;
